f06b7b48ad15c1d98a110c9a6c892db4bc8a0082
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-04-17 06:07:57 -0400

    commit 844103624 on 20200417

--- ma.bash
+++ ma.bash
@@ -9,4 +9,5 @@ shopt -s nullglob globstar
 cd "$JDR"
 # _AT_ username/repository commit
 _AT_ han1202012/CircleProcess f43ee514c211c39ca5e4720ded80e557197a759e
+_AT_ han1202012/AndroidPictureViewer e4377ba55a22bc277e0474d9c82e8fd82b3c32a3
 # ma.bash OEF 
