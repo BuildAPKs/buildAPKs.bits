b4c1ee605a4e547bed2cf1660e2576cea4cc84e3
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-04-17 06:40:30 -0400

    commit 694863177 on 20200417

--- ma.bash
+++ ma.bash
@@ -8,6 +8,7 @@ shopt -s nullglob globstar
 . "$RDR"/scripts/bash/shlibs/trap.bash 210 211 212
 cd "$JDR"
 # _AT_ username/repository commit
-_AT_ han1202012/CircleProcess f43ee514c211c39ca5e4720ded80e557197a759e
 _AT_ han1202012/AndroidPictureViewer e4377ba55a22bc277e0474d9c82e8fd82b3c32a3
+_AT_ han1202012/CircleProcess f43ee514c211c39ca5e4720ded80e557197a759e
+_AT_ han1202012/ZoomButton_QuickContactBadge 9811208f0da56c76ef00bd067bd00f16ef900917
 # ma.bash OEF 
