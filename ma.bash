bcf3c092ca96fc4459ca86744681dd2d788163e1
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-04-17 05:58:40 -0400

    commit 695209953 on 20200417

---
+++ ma.bash
@@ -0,0 +1,12 @@
+#!/bin/env bash
+# Copyright 2019 (c) all rights reserved by BuildAPKs; see LICENSE
+# buildapks.github.io/buildAPKs published courtesy pages.github.com
+# File ~/RDR/conf/README.md has more information about this file.
+#####################################################################
+set -Eeuo pipefail
+shopt -s nullglob globstar
+. "$RDR"/scripts/bash/shlibs/trap.bash 210 211 212
+cd "$JDR"
+# _AT_ username/repository commit
+_AT_ han1202012/CircleProcess f43ee514c211c39ca5e4720ded80e557197a759e
+# ma.bash OEF 
