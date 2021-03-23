b6e4629056239863f44744168c17047f135f1969
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-03-23 11:02:30 -0400

    commit 919042203 on 20210323

--- ma.bash
+++ ma.bash
@@ -11,6 +11,7 @@ cd "$JDR"
 _AT_ blaine/cookbook a7cbb6375f4139313709502c43d9e5879c82f08a
 _AT_ codeskraps/ListTemplate b6363e8b55aee26cb6d3d51cccd7f38b0d94b23f
 _AT_ codeskraps/Chronoid 56eee9ffcb7217a69e34cffcc5b336df0306f6e5
+_AT_ gimite/android-native-exe-demo 07d63e5323996d04096917aa365ab5df8a28434e
 _AT_ han1202012/AndroidPictureViewer e4377ba55a22bc277e0474d9c82e8fd82b3c32a3
 _AT_ han1202012/CircleProcess f43ee514c211c39ca5e4720ded80e557197a759e
 _AT_ han1202012/ZoomButton_QuickContactBadge 9811208f0da56c76ef00bd067bd00f16ef900917
